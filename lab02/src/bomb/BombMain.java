eb4f2c1ac7381f458654c11ff27fa66c23629491
xingting <init.defaultBranch>
2024-03-04 16:56:17 +0800

    finished lab02 bomb

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -11,13 +11,18 @@ public class BombMain {
         // TODO: Find the correct inputs (passwords) to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            b.phase1(IntList.of(0, 9, 3, 0, 8)); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            String x = "0";
+            for (int i = 1; i < 1337; i++){
+                x = x + " 0";
+            }
+            x = x + " -81201430";
+            b.phase2(x);
         }
     }
 }
